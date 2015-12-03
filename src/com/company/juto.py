7b7a2561830548f848693b478639ce9ba48e6147
Felix de Souza <fdesouza@palantir.com>
2015-12-03 19:42:59 +0000

    New cycle approach

--- src/com/company/juto.py
+++ src/com/company/juto.py
@@ -1,13 +1,33 @@
-def better (exp):
-    v = [1, 1, 1, 3]
+starting_window = (0, 0, 1, 1)
+
+def find_cycle_length(exp):
+    # index at which the slow and fast runners meet, this is also the cycle length
+    convergence_point_with_window = find_cycle_convergence_point(exp)
+    cycle_point = calculate_cycle_point(exp, convergence_point_with_window)
+    return cycle_point, convergence_point
+
+def find_cycle_convergence_point(exp):
+    slow, fast = 1, 2
     m = 10 ** exp
-    comp, cur, num_matched = 0,4,0
-    while num_matched != 4:
-        v.append((v[cur-1] + (2*v[cur-3]) % m) % m)
-        if v[cur] == v[comp]:
-            comp += 1
-            num_matched += 1
-        else:
-            comp, num_matched = 0,0
-        cur += 1
-    return cur - 4
+    slow_window = get_next_window(starting_window, m)
+    fast_window = get_next_plus_1_window(starting_window, m)
+    print slow_window, fast_window
+
+    while slow_window != fast_window:
+        slow += 1
+        fast += 2
+        slow_window = get_next_window(slow_window, m)
+        fast_window = get_next_plus_1_window(fast_window, m)
+
+    return slow, slow_window
+
+def get_next_window(window, m):
+    return window[1:] + ((window[3] + (2*window[1] % m)) % m,)
+
+def get_next_plus_1_window(window, m):
+    temp = get_next_window(window, m)
+    return get_next_window(temp, m)
+
+def calculate_cycle_point(exp, point_with_window):
+    convergence_point, window = point_with_window
+    pass
